4b5274e47fc5188296abf4994258ad5d15dd2b19
spv070 <107425398+spv070@users.noreply.github.com>
2022-07-13 08:24:30 -0700

    Add files via upload

---
+++ mbox.js
@@ -0,0 +1,39 @@
+
+adobe.target.getOffers({
+  request: {
+    prefetch: {
+      mboxes: [
+        {
+          index: 0,
+          name: "mbox1"
+        },
+        {
+          index: 1,
+          name: "mbox2"
+        },
+        {
+          index: 2,
+          name: "mbox3"
+        }
+      ]
+    }
+  }
+})
+.then(response => {
+  // get all mboxes from response
+  const mboxes = response.prefetch.mboxes;
+  let count = 1;
+
+  mboxes.forEach(el => {
+    adobe.target.applyOffers({
+      selector: "#container" + count,
+      response: {
+        prefetch: {
+          mboxes: [el]
+        }
+      }
+    });
+
+    count += 1;
+  });
+});
