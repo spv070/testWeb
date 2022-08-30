efb44777097216fd6cd9badd8442d8e12f5309af
spv070 <107425398+spv070@users.noreply.github.com>
2022-08-30 13:05:10 -0700

    Update TargetParam.js

--- TargetParam.js
+++ TargetParam.js
@@ -1,29 +1,14 @@
 <script>
-targetPageParams = function() { ​
+targetPageParams = function() { 
 
-  return { ​
+  return { 
 
-​
+"entity": { 
 
-"entity": { ​
-
-         "id": "<product Id>",​
-
-         "categoryId": "<Product Category Id>"​
-
-} ​
-
-        "profile": { ​
-
-        "storeID": "<User’s selected store Id>",​
-
-         "userID": "<Customer Id for logged-in users>",​
-
-         "loyaltyID": "<User’s loyaltyId>"     ​
-
-      } ​
-
-  }; ​
+         "id": "123456",
+         "categoryId": "abc"
+}
+  };
 
 };
 </script>
