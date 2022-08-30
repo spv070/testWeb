c6ccfcd0129285b5daeb592c134e237e18ef15ce
spv070 <107425398+spv070@users.noreply.github.com>
2022-08-30 12:59:58 -0700

    Add files via upload

---
+++ TargetParam.js
@@ -0,0 +1,29 @@
+<script>
+targetPageParams = function() { ​
+
+  return { ​
+
+​
+
+"entity": { ​
+
+         "id": "<product Id>",​
+
+         "categoryId": "<Product Category Id>"​
+
+} ​
+
+        "profile": { ​
+
+        "storeID": "<User’s selected store Id>",​
+
+         "userID": "<Customer Id for logged-in users>",​
+
+         "loyaltyID": "<User’s loyaltyId>"     ​
+
+      } ​
+
+  }; ​
+
+};
+</script>
