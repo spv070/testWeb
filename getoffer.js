192d09a0e182f0af48a05337bfa800ec9924ba7e
spv070 <107425398+spv070@users.noreply.github.com>
2022-08-31 10:06:09 -0700

    Update getoffer.js

--- getoffer.js
+++ getoffer.js
@@ -1,23 +1,10 @@
 adobe.target.getOffers({
-    request: {
-        execute: {
-            pageLoad: {
-                parameters: {
-"entity.id": '123'
-}
-            }
-        }
-    }
-});
-
-
-adobe.target.getOffers({
   request: {
     "prefetch": {
       "views": [
         {
           "parameters": {
-            "entity.id": "987"
+            "entity.id": "123456"
           },
           "profileParameters": {
             "user-age": 23
