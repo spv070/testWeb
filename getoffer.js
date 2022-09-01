c6bfee94925e9d619dcf6e271fdb87f14e2a647b
subirajrs <subirajrs@gmail.com>
2022-09-01 16:25:41 +0530

    Update getoffer.js

--- getoffer.js
+++ getoffer.js
@@ -7,34 +7,7 @@ adobe.target.getOffer({
   "success": function(offer) {
           var result =Object.entries(offer);
     console.log(result[0][1].offer[0]);
-                adobe.target.applyOffers({response:{
-  "execute": {
-    "pageLoad": {
-      "options": [{
-        "type": "html",
-        "content": "page-load"
-      },
-      {
-       
-        "type": "insertAfter",
-        "content": [{
-          "type": "setHtml",
-          "content": "<h1>Container 1</h1>",
-          "selector": "HTML > BODY",
-        
-        }]
-      }],
- 
- 
-      "metrics": [{
-        "type": "click",
-        "selector": "HTML > BODY",
-        "eventToken": "page-load-click-metric" 
-      }]
-    }
-  }
-}});
-
+               
   },
   "error": function(status, error) {
     console.log('Error', status, error);
