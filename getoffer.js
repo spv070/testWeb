6d544364d7328f34b04715a70f333d0bb42362ae
subirajrs <subirajrs@gmail.com>
2022-09-01 17:07:06 +0530

    Update getoffer.js

--- getoffer.js
+++ getoffer.js
@@ -7,7 +7,7 @@ adobe.target.getOffer({
   "success": function(offer) {
     console.log("Result: "+JSON.stringify(offer));
           var result =Object.entries(offer);
-    console.log(result[0][1].content);
+    console.log("content is"+result[0][1].content);
     
     adobe.target.applyOffers({response:{
   "execute": {
