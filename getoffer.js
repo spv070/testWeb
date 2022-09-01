a9504970e3f006e2aa1e085ef8c9e399edbb575b
subirajrs <subirajrs@gmail.com>
2022-09-01 14:42:27 +0530

    Update getoffer.js

--- getoffer.js
+++ getoffer.js
@@ -6,9 +6,9 @@ adobe.target.getOffer({
      "entity.name":"My Awesome Product 1",
      "entity.message":"One of our best products!"
   },
-  "success": function(offer) {
-  console.log("Result: "+JSON.stringify(offer));
-    var sample=JSON.stringify(offer);
+  "success": function(content) {
+  
+    var sample=JSON.stringify(content);
     console.log(sample);
     console.log(sample.action);
 
